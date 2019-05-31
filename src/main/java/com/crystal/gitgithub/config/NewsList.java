5c63b9bf5ad4e482fcba193b5998c9ecebfada63
Crystalqt <qutao1205@163.com>
2019-05-31 18:56:08 +0800

    First commit - git github test

---
+++ src/main/java/com/crystal/gitgithub/config/NewsList.java
@@ -0,0 +1,10 @@
+package com.crystal.gitgithub.config;
+
+/**
+ * @ClassName;NewsList
+ * @Author;qutao
+ * @Date;2019/5/31 6:09 PM
+ * @Discription; TODO
+ */
+public class NewsList {
+}
---
+++ src/main/java/com/crystal/gitgithub/config/Swagger2Config.java
@@ -0,0 +1,10 @@
+package com.crystal.gitgithub.config;
+
+/**
+ * @ClassName;Swagger2Config
+ * @Author;qutao
+ * @Date;2019/5/31 11:42 AM
+ * @Discription; TODO
+ */
+public class Swagger2Config {
+}
